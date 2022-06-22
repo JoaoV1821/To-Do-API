2b9c2933c59a69b63c6be1b3a94ed1cb2ef95921
João Vitor <joaovitoraraujo1821@gmail.com>
2022-06-22 19:28:41 -0300

    iniciei a rota put

--- src/app.js
+++ src/app.js
@@ -2,21 +2,9 @@ import  Express  from 'express';
 
 import { userGet }   from './controller/userController.js';
 import { taskGet }   from './controller/taskController.js';
-import { UserModel } from './model/UserModel.js';
-import { TaskModel } from './model/taskModel.js';
 
 const port = 3000;
 const app = Express();
-
-const user = new UserModel('João', 'email@gmail.com', '12345');
-const task = new TaskModel('Titulo' ,'feito', 'descricao', '20-06-2022');
-
-userGet(app);
-taskGet(app);
-
-
-console.log(user)
-console.log(task)
 
 app.use(Express.json());
 
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -7,10 +7,61 @@ export const userGet = (app) => {
     });
 
     app.post('/user', (req, res) => {
-        const body = req.body;
-        const user = new UserModel(body.nome, body.email, body.senha);
-        
+        const user = new UserModel(req.body.nome, req.body.email, req.body.senha);
+
         bd.Alunos.push(user);
-        res.send(task);
+        res.send(user);
+    });
+
+    app.get('/user/:email', (req, res) => {
+        const email = req.params.email;
+
+        for (let i = 0; i < bd.Alunos.length; i++) {
+
+            if (email == bd.Alunos[i].email) {
+                res.send(email);
+            } else {
+                res.send('Email não encontrado');
+            }
+        }
+    });
+
+    app.delete('/user/:email', (req, res) => {
+        const email = req.params.email;
+        const emailIndex = bd.Alunos.findIndex(aluno => aluno.email == email);
+        if (emailIndex > -1) {
+            bd.Alunos.splice(emailIndex, 1);
+
+            res.json({
+                "Usuário excluído": bd.Alunos[emailIndex],
+                "Posição no banco": emailIndex
+            });
+        } else {
+            res.send('Aluno não encontrado')
+        }
+    });
+
+    app.put('/user/:email', (req, res) => {
+        const email = req.params.email;
+        
+        for (let i=0; i<=bd.Alunos.length; i++) {
+            const dadoNovo = req.body;
+            const dadoAntigo = bd.Alunos[i]
+
+            if (email == bd.Alunos[i].email) {
+                
+                const user = new UserModel(
+                    dadoAntigo.nome || dadoNovo.aluno, dadoAntigo.email || dadoNovo.email, dadoAntigo.senha || dadoNovo.senha
+                );
+
+              const userAlterado = bd.Alunos.shift(i,1, user);
+
+              res.json({"Aluno antigo": dadoAntigo, "Aluno novo": userAlterado });
+
+            } else {
+                res.send('Aluno não encontrado')
+            };
+        }; 
+
     });
 };
